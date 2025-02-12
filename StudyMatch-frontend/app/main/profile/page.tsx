89d80596f4961bd2fd30ddba5502aa53077a3c1b
mimii020 <imen.abidi@insat.ucar.tn>
2025-02-12 13:37:57 -0500

    added student module in the backend

--- StudyMatch-frontend/app/main/profile/page.tsx
+++ StudyMatch-frontend/app/main/profile/page.tsx
@@ -4,11 +4,11 @@ import React from 'react'
 
 function page() {
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-full relative">
       <div className="w-full h-1/3 bg-purple-300"></div>
-      <div className="w-48 h-48 bg-slate-400 rounded-full"></div>
-      <h1 className="font-extrabold text-center mt-48 text-black">Mimi</h1>
-      <ProfileSection title="About Me" topMargin="100px">
+      <div className="w-48 h-48 bg-slate-400 rounded-full absolute top-[20%] left-1/2 -translate-x-1/2"></div>
+      <h1 className="font-extrabold text-center mt-32 text-black">Mimi</h1>
+      <ProfileSection title="About Me" topMargin="80px">
         <p>heyy this is me your favorite girliee</p>
       </ProfileSection>
 
